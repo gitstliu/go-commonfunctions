d5d8de831eefe8342149905ddc297e904f84320c
Steven <35721846+gitstliu@users.noreply.github.com>
2022-05-16 22:42:57 +0800

    Update commonfunctions.go

--- commonfunctions.go
+++ commonfunctions.go
@@ -10,6 +10,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"errors"
+	"strconv"
 
 	"github.com/gitstliu/log4go"
 )
@@ -154,4 +156,77 @@ func IsGzipEncode(header http.Header) bool {
 	value := header["Content-Encoding"]
 
 	return value != nil && strings.EqualFold(value[0], "gzip")
+}
+
+func ValsToStrs(values []interface{}) ([]string, error) {
+	result := []string{}
+	for index, value := range values {
+		var key string
+		if value == nil {
+			return nil, errors.New(fmt.Sprintf("index %d is nil!!", index))
+		}
+
+		switch value.(type) {
+		case float64:
+			ft := value.(float64)
+			key = strconv.FormatFloat(ft, 'f', -1, 64)
+		case float32:
+			ft := value.(float32)
+			key = strconv.FormatFloat(float64(ft), 'f', -1, 64)
+		case int:
+			it := value.(int)
+			key = strconv.Itoa(it)
+		case uint:
+			it := value.(uint)
+			key = strconv.Itoa(int(it))
+		case int8:
+			it := value.(int8)
+			key = strconv.Itoa(int(it))
+		case uint8:
+			it := value.(uint8)
+			key = strconv.Itoa(int(it))
+		case int16:
+			it := value.(int16)
+			key = strconv.Itoa(int(it))
+		case uint16:
+			it := value.(uint16)
+			key = strconv.Itoa(int(it))
+		case int32:
+			it := value.(int32)
+			key = strconv.Itoa(int(it))
+		case uint32:
+			it := value.(uint32)
+			key = strconv.Itoa(int(it))
+		case int64:
+			it := value.(int64)
+			key = strconv.FormatInt(it, 10)
+		case uint64:
+			it := value.(uint64)
+			key = strconv.FormatUint(it, 10)
+		case string:
+			key = value.(string)
+		case []byte:
+			key = string(value.([]byte))
+		default:
+			newValue, _ := json.Marshal(value)
+			key = string(newValue)
+		}
+
+		result = append(result, key)
+	}
+
+	return result, nil
+
+}
+
+func StringInStrings(values []string, value string) int {
+	result := -1
+
+	for index, currValue := range values {
+		if currValue == value {
+			result = index
+			return result
+		}
+	}
+	return result
 }
